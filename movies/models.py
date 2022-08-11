566a1b36b7d69092043e10999d070c0f89797830
npocbet <npocbet@gmail.com>
2022-08-11 17:47:16 +0300

    testing migrations

--- movies/models.py
+++ movies/models.py
@@ -50,6 +50,12 @@ class Genre(UUIDMixin, TimeStampedMixin):
 class Person(UUIDMixin, TimeStampedMixin):
     full_name = models.CharField(_('full_name'), max_length=255)
 
+    class Gender(models.TextChoices):
+        MALE = 'male', _('male')
+        FEMALE = 'female', _('female')
+
+    gender = models.TextField(_('gender'), choices=Gender.choices, null=True)
+
     class Meta:
         # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
         db_table = "content\".\"person"
@@ -69,6 +75,10 @@ class FilmWork(UUIDMixin, TimeStampedMixin):
     rating = models.FloatField(_('rating'), blank=True,
                                validators=[MinValueValidator(0),
                                            MaxValueValidator(100)])
+    certificate = models.CharField(_('certificate'), max_length=512, blank=True)
+    # Параметр upload_to указывает, в какой подпапке будут храниться загружемые файлы.
+    # Базовая папка указана в файле настроек как MEDIA_ROOT
+    file_path = models.FileField(_('file'), blank=True, null=True, upload_to='movies/')
 
     class Type(models.TextChoices):
         m = "m", "movie"
@@ -110,5 +120,6 @@ class PersonFilmwork(UUIDMixin):
     person = models.ForeignKey('Person', on_delete=models.CASCADE)
     role = models.TextField(_('role'), null=True)
     created = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         db_table = "content\".\"person_film_work"
