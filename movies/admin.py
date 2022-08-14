72ddea84610fad9dedea663de4f184e86e4ea3fb
npocbet <npocbet@gmail.com>
2022-08-14 11:15:02 +0300

    2 steps of 1st sprint ended, but need 2 fix migrations

--- movies/admin.py
+++ movies/admin.py
@@ -1,19 +1,26 @@
 from django.contrib import admin
-from .models import Genre, FilmWork, GenreFilmwork
+from .models import Genre, FilmWork, GenreFilmwork, PersonFilmwork, Person
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    pass
 
 class GenreFilmworkInline(admin.TabularInline):
     model = GenreFilmwork
 
 
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
+
+
 @admin.register(FilmWork)
 class FilmworkAdmin(admin.ModelAdmin):
-    inlines = (GenreFilmworkInline,)
+    inlines = (GenreFilmworkInline, PersonFilmworkInline)
     # Отображение полей в списке
     list_display = ('title', 'type', 'creation_date', 'rating',)
     # Фильтрация в списке
