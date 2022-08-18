b2ec2c143587248e7cea086315315344e580e499
npocbet <npocbet@gmail.com>
2022-08-18 15:26:03 +0300

    1. removed all empty files 2. remove all unnecessary comments 3. added description of foreign keys in models.py 4. added some additional fields in admin.py

--- movies/admin.py
+++ movies/admin.py
@@ -4,25 +4,35 @@ from .models import Genre, FilmWork, GenreFilmwork, PersonFilmwork, Person
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['genre']
+
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['person']
+
 
 class GenreFilmworkInline(admin.TabularInline):
     model = GenreFilmwork
+    autocomplete_fields = ['genre']
 
 
 class PersonFilmworkInline(admin.TabularInline):
     model = PersonFilmwork
+    autocomplete_fields = ['person']
 
 
 @admin.register(FilmWork)
 class FilmworkAdmin(admin.ModelAdmin):
+
+    def get_genres(self, obj):
+        return ','.join([g.name for g in obj.genre.all()])
+
+    get_genres.short_description = 'Жанры фильма'
+
     inlines = (GenreFilmworkInline, PersonFilmworkInline)
     # Отображение полей в списке
-    list_display = ('title', 'type', 'creation_date', 'rating',)
+    list_display = ('title', 'type', 'creation_date', 'rating', 'get_genres',)
     # Фильтрация в списке
     list_filter = ('type',)
     # Поиск по полям
--- movies/models.py
+++ movies/models.py
@@ -6,7 +6,9 @@ import uuid
 
 
 class TimeStampedMixin(models.Model):
+    # auto_now_add автоматически выставит дату создания записи
     created = models.DateTimeField(auto_now_add=True)
+    # auto_now изменятся при каждом обновлении записи
     modified = models.DateTimeField(auto_now=True)
 
     class Meta:
@@ -21,27 +23,20 @@ class UUIDMixin(models.Model):
         abstract = True
 
 
-# Create your models here.
 class Genre(UUIDMixin, TimeStampedMixin):
-    # Типичная модель в Django использует число в качестве id. В таких ситуациях поле не описывается в модели.
-    # Вам же придётся явно объявить primary key.
-    ###    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # Первым аргументом обычно идёт человекочитаемое название поля
     name = models.CharField(_('name'), max_length=255)
     # blank=True делает поле необязательным для заполнения.
     description = models.TextField(_('description'), blank=True)
-
-    # auto_now_add автоматически выставит дату создания записи
-    ###    created = models.DateTimeField(auto_now_add=True)
-    # auto_now изменятся при каждом обновлении записи
-    ###    modified = models.DateTimeField(auto_now=True)
 
     class Meta:
         # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
         db_table = "content\".\"genre"
         # Следующие два поля отвечают за название модели в интерфейсе
         verbose_name = 'Жанр'
-        verbose_name_plural = 'Жанры'  # Create your models here.
+        verbose_name_plural = 'Жанры'
+        indexes = [
+            models.Index(fields=['id'], name='genre_id_idx'),
+        ]
 
     def __str__(self):
         return self.name
@@ -50,35 +45,25 @@ class Genre(UUIDMixin, TimeStampedMixin):
 class Person(UUIDMixin, TimeStampedMixin):
     full_name = models.CharField(_('full_name'), max_length=255)
 
-    # class Gender(models.TextChoices):
-    #     MALE = 'male', _('male')
-    #     FEMALE = 'female', _('female')
-    #
-    # gender = models.TextField(_('gender'), choices=Gender.choices, null=True)
-
     class Meta:
-        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
         db_table = "content\".\"person"
-        # Следующие два поля отвечают за название модели в интерфейсе
         verbose_name = 'Человек'
-        verbose_name_plural = 'Люди'  # Create your models here.
+        verbose_name_plural = 'Люди'
+        indexes = [
+            models.Index(fields=['id'], name='person_id_idx'),
+        ]
 
     def __str__(self):
         return self.full_name
 
 
 class FilmWork(UUIDMixin, TimeStampedMixin):
-    ###    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(_('title'), max_length=255)
     description = models.TextField(_('description'), blank=True)
     creation_date = models.DateField()
     rating = models.FloatField(_('rating'), blank=True,
                                validators=[MinValueValidator(0),
                                            MaxValueValidator(100)])
-    # certificate = models.CharField(_('certificate'), max_length=512, blank=True)
-    # Параметр upload_to указывает, в какой подпапке будут храниться загружемые файлы.
-    # Базовая папка указана в файле настроек как MEDIA_ROOT
-    # file_path = models.FileField(_('file'), blank=True, null=True, upload_to='movies/')
 
     class Type(models.TextChoices):
         m = "m", "movie"
@@ -89,15 +74,13 @@ class FilmWork(UUIDMixin, TimeStampedMixin):
         choices=Type.choices
     )
 
-    ###    created = models.DateTimeField(auto_now_add=True)
-    ###    modified = models.DateTimeField(auto_now=True)
-
     class Meta:
-        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
         db_table = "content\".\"film_work"
-        # Следующие два поля отвечают за название модели в интерфейсе
         verbose_name = 'Основная информация о кинопроизведении'
         verbose_name_plural = 'Основная информация о кинопроизведении'
+        indexes = [
+            models.Index(fields=['id'], name='film_work_id_idx'),
+        ]
 
     def __str__(self):
         return self.title
@@ -107,19 +90,36 @@ class FilmWork(UUIDMixin, TimeStampedMixin):
 
 
 class GenreFilmwork(UUIDMixin):
-    film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
-    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    film_work = models.ForeignKey(FilmWork, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = "content\".\"genre_film_work"
+        indexes = [
+            models.Index(fields=['film_work', 'genre'], name='film_work_genre_idx'),
+        ]
 
 
 class PersonFilmwork(UUIDMixin):
-    film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
-    person = models.ForeignKey('Person', on_delete=models.CASCADE)
+    film_work = models.ForeignKey(FilmWork, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    class Role(models.TextChoices):
+        m = "d", "director"
+        t = "s", "screenwriter"
+        a = "a", "actor"
+
+    role = models.CharField(
+        max_length=12,
+        choices=Role.choices
+    )
+
     role = models.TextField(_('role'), null=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = "content\".\"person_film_work"
+        indexes = [
+            models.Index(fields=['film_work', 'person'], name='film_work_person_idx'),
+        ]
