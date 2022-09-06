8b869316e3adde087ff6631fd8a4a03197fb7073
npocbet <npocbet@gmail.com>
2022-09-06 15:15:13 +0300

    Решены оба задания 2 спринта 1. docker-compose.yaml в nginx добавлено подключение статики 2. nginx.conf добавлена настройка отключения отображения версий 3. site.conf подключена статика из nginx 4. settings.py подключена статика, добавлена настройка для корректной работы swagger и возможность отладки api через django_extensions

--- config/settings.py
+++ config/settings.py
@@ -40,11 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movies.apps.MoviesConfig',
+    'django_extensions',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -53,6 +56,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8080", ]
 
 TEMPLATES = [
     {
@@ -71,7 +75,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -114,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = 'src/static'
 STATIC_URL = '/static/'
 
 # Default primary key field type
@@ -133,7 +137,36 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT', 5432),
         'OPTIONS': {
             # Нужно явно указать схемы, с которыми будет работать приложение.
-           'options': '-c search_path=public,content'
+            'options': '-c search_path=public,content'
         }
     }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]',
+        },
+    },
+    'handlers': {
+        'debug-console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+            'filters': ['require_debug_true'],
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['debug-console'],
+            'propagate': False,
+        }
+    },
 }
---
+++ movies/api/v1/views.py
@@ -0,0 +1,65 @@
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import Q
+from django.http import JsonResponse
+from django.views.generic.detail import BaseDetailView
+from django.views.generic.list import BaseListView
+
+from movies.models import FilmWork
+
+
+class MoviesApiMixin:
+    model = FilmWork
+    http_method_names = ['get']
+    paginate_by = 50
+
+    def get_queryset(self):
+        films = FilmWork.objects.prefetch_related('genres', 'persons', ) \
+            .annotate(genres=ArrayAgg('genre__name', distinct=True),
+                      actors=ArrayAgg('person__full_name', filter=Q(personfilmwork__role='actor'),
+                                      distinct=True),
+                      directors=ArrayAgg('person__full_name', filter=Q(personfilmwork__role='director'),
+                                         distinct=True),
+                      writers=ArrayAgg('person__full_name', filter=Q(personfilmwork__role='writer'),
+                                       distinct=True)) \
+            .values('id', 'title', 'description', 'creation_date', 'rating', 'type', 'genres',
+                    'actors', 'directors', 'writers')
+        return films
+
+
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
+
+
+class MoviesListApi(MoviesApiMixin, BaseListView):
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        queryset = self.get_queryset()
+        paginator, page, queryset, is_paginated = self.paginate_queryset(
+            queryset,
+            self.paginate_by
+        )
+        return {'count': paginator.count,
+                'total_pages': paginator.num_pages,
+                'prev': page.previous_page_number() if page.has_previous() else None,
+                'next': page.next_page_number() if page.has_next() else None,
+                'results': list(page.object_list.values('id', 'title', 'description', 'creation_date',
+                                                        'rating', 'type', 'genres', 'actors',
+                                                        'directors', 'writers'))
+                }
+
+
+class MoviesDetailApi(MoviesApiMixin, BaseDetailView):
+
+    def get_context_data(self, **kwargs):
+
+        return {'id': kwargs['object']['id'],
+                'title': kwargs['object']['title'],
+                'description': kwargs['object']['description'],
+                'creation_date': kwargs['object']['creation_date'],
+                'rating': kwargs['object']['rating'],
+                'type': kwargs['object']['type'],
+                'genres': kwargs['object']['genres'],
+                'actors': kwargs['object']['actors'],
+                'directors': kwargs['object']['directors'],
+                'writers': kwargs['object']['writers']
+                }
